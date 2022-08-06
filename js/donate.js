3142ba85fc889e2d186c4811dd906fe520531820
Umesh Kumawat <68832856+Royanosh@users.noreply.github.com>
2022-08-06 14:47:36 +0530

    donate js

--- js/donate.js
+++ js/donate.js
@@ -1,26 +1,45 @@
 document.querySelector(".homebtn").addEventListener("click", function () {
-  location.href = "index.html";
+  location.href = "../index.html";
 });
 document.querySelector(".donatebtn").addEventListener("click", function () {
   location.href = "donate.html";
 });
 
 document.querySelector(".reviewbtn").addEventListener("click", function () {
-  location.href = "review.html";
+  location.href = "../review.html";
 });
 
 document.querySelector(".pricingbtn").addEventListener("click", function () {
-  location.href = "pricing.html";
+  location.href = "../pricing.html";
 });
 
 document.querySelector(".contactusbtn").addEventListener("click", function () {
-  location.href = "contactUs.html";
+  location.href = "../contactUs.html";
 });
 
 document.querySelector(".loginbtn").addEventListener("click", function () {
-  location.href = "Signin.html";
+  location.href = "../Signin.html";
 });
 
 document.querySelector(".signupbtn").addEventListener("click", function () {
   location.href = "../SignUp.html";
 });
+
+document.getElementById("LOSINIMG").addEventListener("click", LOginFUN);
+var ddaa = document.getElementById("LOSIN");
+function LOginFUN() {
+  if (ddaa.style.display === "none") {
+    ddaa.style.display = "flex";
+    ddaa.style.justifyContent = "space-around";
+    // ddaa.style.margin= "0";
+    ddaa.style.paddingTop = "15px";
+  } else {
+    ddaa.style.display = "none";
+  }
+}
+
+document.getElementById("divCLICK").addEventListener("click", changePage);
+
+function changePage() {
+  location.href = "divSection/div1.html";
+}
